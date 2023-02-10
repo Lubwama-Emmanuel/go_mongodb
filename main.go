7e29b11702f6f9d1fc2b7bc389bea926eed07232
Rex Munil <lubwamaemmanuel1@gmail.com>
2023-02-10 22:07:31 +0300

    intial push

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"net/http"
+
+	"github.com/Lubwama-Emmanuel/go_mongodb/controllers"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("/users/:id", uc.GetUser)
+	r.POST("/users", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+}
+
+func getSession() *mgo.Session {
+	s, err := mgo.Dial("mongodb://localhost:27017")
+	if err != nil {
+		fmt.Println("An error occured", err)
+		panic(err)
+	}
+	return s
+
+}
